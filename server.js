babdd3b67ad188a75cb1f6239898388cb8516a8a
Tamosa Chakraborty <ctamo.net@gmail.com>
2024-08-14 14:37:16 +0530

    m2

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const bodyParser = require('body-parser');
 const express = require('express');
-const mysql = require('mysql'); // Assuming you're using MySQL
+const mysql = require('mysql2');
+const path=require('path'); // Assuming you're using MySQL
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname,"public")));
 const port = 8080;
 
 // Create a connection to the database
@@ -9,9 +13,18 @@ const db = mysql.createConnection({
   host: 'database-2.cvgio2aq09cd.ap-south-1.rds.amazonaws.com',
   user: 'admin',
   password: 'Tamosa1985',
-  database: 'faculty'
+  database: 'it_department'
 });
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 8);
 
+  const sql = `INSERT INTO users (username, password) VALUES (?, ?)`;
+  db.query(sql, [username, hashedPassword], (err, result) => {
+    if (err) throw err;
+    res.send('User registered');
+  });
+});
 // Connect to the database
 db.connect(err => {
   if (err) {
@@ -24,13 +37,17 @@ db.connect(err => {
 // Define the route
 app.get('/users', (req, res) => {
   console.log("GET /users route accessed"); // Debugging line
-  db.query('SELECT * FROM users', (err, results) => {
+  db.query('SELECT * FROM faculty', (err, results) => {
       if (err) {
           console.error("Error fetching users:", err); // Debugging line
           return res.status(500).json({ error: 'Failed to fetch users' });
       }
       res.json(results);
   });
+});
+
+app.get('/',(req,res)=>{
+  res.sendFile(path.join(__dirname,'index.html'))
 });
 
 // Start the server
